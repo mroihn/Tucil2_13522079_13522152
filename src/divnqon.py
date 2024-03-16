994840c01ec92057f870892f867e9b949832542a
mroihn <116539684+mroihn@users.noreply.github.com>
2024-03-16 12:59:21 +0700

    Feat divide qonquer , visualisasi

---
+++ src/divnqon.py
@@ -0,0 +1,71 @@
+import time
+
+from plotting import *
+
+curve_points = []
+control_points = []
+mid_points = []
+iteration = 0
+x_points = []
+y_points = []
+
+
+# Fungsi untuk mendapatkan titik kurva
+def solve(p0, p1, p2):
+    global curve_points
+    curve_points.append(p0)
+    divide_conquer(p0, p1, p2, 0)
+    curve_points.append(p2)
+
+
+# Fungsi divide and conquer
+def divide_conquer(p0, p1, p2, iterationNow):
+    global curve_points
+    if iterationNow < iteration:
+        mid1 = [(p0[0] + p1[0]) / 2, (p0[1] + p1[1]) / 2]
+        mid2 = [(p1[0] + p2[0]) / 2, (p1[1] + p2[1]) / 2]
+        mid = [(mid1[0] + mid2[0]) / 2, (mid1[1] + mid2[1]) / 2]
+        mid_points.append(mid1)
+        mid_points.append(mid2)
+        mid_points.append(mid)
+
+        iterationNow += 1
+
+        # kiri
+        divide_conquer(p0, mid1, mid, iterationNow)
+
+        curve_points.append(mid)
+
+        # kanan
+        divide_conquer(mid, mid2, p2, iterationNow)
+
+
+def main():
+    global iteration
+    # input titik kontrol
+    print("Masukkan control point: ")
+    p0 = tuple(map(int, input().split(" ")))
+    p1 = tuple(map(int, input().split(" ")))
+    p2 = tuple(map(int, input().split(" ")))
+
+    # input jumlah iterasi
+    iteration = int(input("Masukkan jumlah iterasi : "))
+    control_points.append(p0)
+    control_points.append(p1)
+    control_points.append(p2)
+
+    start_time = time.time()
+    solve(p0, p1, p2)
+    end_time = time.time()
+    execution_time = end_time - start_time
+
+    print("Bezier Curve Points:")
+    for point in curve_points:
+        print(point)
+
+    print("\nExecution Time:", execution_time, "seconds")
+    plot_curve(control_points, mid_points, curve_points)
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/plotting.py
@@ -0,0 +1,58 @@
+import matplotlib.pyplot as plt
+
+
+def plot_curve(control_points, mid_points, curve_points):
+    global x_points, y_points
+    x_points = []
+    y_points = []
+    for step_points in control_points:
+        x_points.append(step_points[0])
+        y_points.append(step_points[1])
+
+        plt.title("Quadratic Bezier Curve (Step by Step)")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.grid(True)
+        plt.scatter(x_points, y_points, color="red")
+        plt.pause(0.25)
+        plt.plot(x_points, y_points, "-o", color="black", markerfacecolor="red")
+        plt.pause(0.25)
+    plot_mid_point(mid_points)
+    plot_curve_point(curve_points)
+    plt.show()
+
+
+def plot_mid_point(mid_points):
+    global x_points, y_points
+    x_points = []
+    y_points = []
+    for step_points in mid_points:
+        x_points.append(step_points[0])
+        y_points.append(step_points[1])
+
+        plt.title("Quadratic Bezier Curve (Step by Step)")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.grid(True)
+        plt.scatter(x_points, y_points, color="grey")
+        plt.pause(0.25)
+        plt.plot(x_points, y_points, "-o", color="silver", markerfacecolor="grey")
+        plt.pause(0.25)
+
+
+def plot_curve_point(curve_points):
+    global x_points, y_points
+    x_points = []
+    y_points = []
+    for step_points in curve_points:
+        x_points.append(step_points[0])
+        y_points.append(step_points[1])
+
+        plt.title("Quadratic Bezier Curve (Step by Step)")
+        plt.xlabel("X-axis")
+        plt.ylabel("Y-axis")
+        plt.grid(True)
+        plt.scatter(x_points, y_points, color="gold")
+        plt.pause(0.25)
+        plt.plot(x_points, y_points, "-o", color="yellow", markerfacecolor="gold")
+        plt.pause(0.25)
